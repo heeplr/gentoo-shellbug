123b3934ab64ead88876767a868336bb92a6b32b
daniel <daniel@localhost>
2022-03-21 11:45:55 +0100

    write textfiles

--- bugreports.py
+++ bugreports.py
@@ -3,14 +3,18 @@
 # generate bugreports from output.json
 
 import json
+import os
 
 import shellbug
 
 
 ISSUEFILE="output.json"
-OUTDIR="out"
+OUTDIR="./out"
 
 if __name__ == "__main__":
+
+    # create outdir if not existing
+    os.makedirs(OUTDIR, exist_ok=True)
 
     # load issues
     with open(ISSUEFILE) as fd:
@@ -24,16 +28,16 @@ if __name__ == "__main__":
               f"To minimize false-positive findings, conservative settings were used. " \
               f"You might want to re-run shellcheck and check the complete output for more issues.\n" \
               f"Please check the findings below. A link to the shellcheck wiki " \
-              f"will provide further details on every issue found.\n"
+              f"will provide further details.\n"
 
         for issue in issuelist:
             msg += "\n------------------------------------------------\n"
             msg += shellbug.result_to_str(issue)
 
-        msg += "\nThis is an automated bug report.\n"
+        msg += "\nThis is an automated bug report.\n\n"
         msg += "References:\n"
         msg += "[1] this script: https://github.com/heeplr/gentoo-shellbug\n"
         msg += "[2] shellcheck: https://www.shellcheck.net/"
 
-        print(msg)
-        break
+        with open(f"{OUTDIR}/{package.replace('/', '_')}.txt", "w") as fd:
+            fd.write(msg)
